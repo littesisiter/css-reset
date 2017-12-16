36f07ff0ac59e6470dea68e5ec1179beb4b3c72d
qiaodaima <674059309@qq.com>
2017-12-16 14:02:58 +0800

    改变目录结构

---
+++ README.md
@@ -0,0 +1,24 @@
+# 使用说明
+
+> 需要 `node` 环境，并且全局安装了 `gulp`
+
+## 涉及的插件
+```js
+browser-sync              自动刷新，静态服务
+gulp-load-plugins         自动加载gulp插件
+gulp-autoprefixer         设置浏览器版本自动处理浏览器前缀
+gulp-csso                 压缩优化css
+gulp-sass                 编译sass
+gulp-sourcemaps           生层map文件
+gulp-clean                删除文件
+```
+
+## 初次运行
+```js
+npm install 
+```
+
+## 启动
+```js
+gulp default 
+```
---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+/**
+* 以【gulp】开头的组件 在安装后已经自动加载，无须再引入，
+* 当使用【gulp-rename】和【gulp-ruby-sass】这两个插件的时候，
+* 就可以使用【$.rename】和【$.rubySass】来代替了,
+* 也就是原始插件名【去掉gulp-前缀】，改成【$.】，之后再转换为驼峰命名。
+*/
+var gulp        = require('gulp');
+var $           = require('gulp-load-plugins')();
+var browserSync = require('browser-sync').create();
+
+
+
+/**
+* 编译sass、添加浏览器前缀、生成map文件
+* http://browserl.ist   可以在这个页面输入查询参数
+*/
+gulp.task('sass', ['cleanDistFolder'], function() {
+    return gulp.src('./src/*.scss')
+               .pipe($.sourcemaps.init())
+               .pipe($.sass({outputStyle: 'expanded'}).on('error', $.sass.logError))
+               .pipe($.autoprefixer({
+                    browsers: ['last 4 Explorer versions', 'Chrome >= 0', 'Firefox >= 0', 'Opera >= 0'],
+                    cascade: true,
+                    remove: true
+               }))
+               .pipe($.csso())
+               .pipe($.sourcemaps.write('./dist/'))
+               .pipe(gulp.dest('./dist'))
+               .pipe(browserSync.reload({stream:true}));
+});
+gulp.task('cleanDistFolder', function(){
+    return gulp.src('./dist/')
+               .pipe($.clean());
+});
+gulp.watch('./src/*.scss', ['sass']);
+
+
+
+
+/**
+* 默认任务
+* 1.清空产出目录
+* 2.产出相关目录 这个是异步的，互不干扰
+* 3.启动静态服务器
+*/
+gulp.task('default', ['sass'], function(){
+    return browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+});
